c9cdfc34fb9fe8a5072136e01fbf1d2fbf2fb911
Rishi Srivastava <rishisrivasta@gmail.com>
2022-08-24 16:58:29 +0800

    started with 2 problems

---
+++ src/main/java/com/backstreetbrogrammer/dynamic/top20/LongestCommonSubsequence.java
@@ -0,0 +1,43 @@
+package com.backstreetbrogrammer.dynamic.top20;
+
+/**
+ * https://leetcode.com/problems/longest-common-subsequence/
+ * <p>
+ * Given two strings text1 and text2, return the length of their longest common subsequence. If there is no common subsequence, return 0.
+ * <p>
+ * A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
+ * <p>
+ * For example, "ace" is a subsequence of "abcde".
+ * A common subsequence of two strings is a subsequence that is common to both strings.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: text1 = "abcde", text2 = "ace"
+ * Output: 3
+ * Explanation: The longest common subsequence is "ace" and its length is 3.
+ */
+public class LongestCommonSubsequence {
+
+    public static void main(final String[] args) {
+        System.out.println(longestCommonSubsequence("abcde", "ace"));
+    }
+
+    private static int longestCommonSubsequence(final String text1, final String text2) {
+        final int[][] dp = new int[text1.length() + 1][text2.length() + 1];
+
+        for (int i = text1.length() - 1; i >= 0; i--) {
+            for (int j = text2.length() - 1; j >= 0; j--) {
+                if (text1.charAt(i) == text2.charAt(j)) {
+                    dp[i][j] = 1 + dp[i + 1][j + 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i][j + 1], dp[i + 1][j]);
+                }
+            }
+        }
+
+        return dp[0][0];
+    }
+
+}
---
+++ src/main/java/com/backstreetbrogrammer/dynamic/top20/LongestIncreasingSubsequence.java
@@ -0,0 +1,39 @@
+package com.backstreetbrogrammer.dynamic.top20;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/longest-increasing-subsequence/
+ * <p>
+ * Given an integer array nums, return the length of the longest strictly increasing subsequence.
+ * <p>
+ * A subsequence is a sequence that can be derived from an array by deleting some or no elements without changing the order of the remaining elements. For example, [3,6,2,7] is a subsequence of the array [0,3,1,6,2,2,7].
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [10,9,2,5,3,7,101,18]
+ * Output: 4
+ * Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+ */
+public class LongestIncreasingSubsequence {
+
+    public static void main(final String[] args) {
+        System.out.println(lengthOfLIS(new int[]{10, 9, 2, 5, 3, 7, 101, 18}));
+    }
+
+    private static int lengthOfLIS(final int[] nums) {
+        final int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] < nums[j]) {
+                    dp[i] = Math.max(dp[i], 1 + dp[j]);
+                }
+            }
+        }
+
+        return Arrays.stream(dp).max().getAsInt();
+    }
+}
