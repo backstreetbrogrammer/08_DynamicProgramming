712bc11827175dd03709dd72bbab978513c1f275
Rishi Srivastava <rishisrivasta@gmail.com>
2022-11-24 07:34:04 +0800

    added snapshot for problem 13

--- src/main/java/com/backstreetbrogrammer/dynamic/PartitionEqualSubsetSum.java
+++ src/main/java/com/backstreetbrogrammer/dynamic/PartitionEqualSubsetSum.java
@@ -4,6 +4,17 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * https://leetcode.com/problems/partition-equal-subset-sum/
+ * <p>
+ * Given a non-empty array nums containing only positive integers, find if the array can be partitioned into two subsets
+ * such that the sum of elements in both subsets is equal.
+ * <p>
+ * Example:
+ * Input: nums = [1,5,11,5]
+ * Output: true
+ * Explanation: The array can be partitioned as [1, 5, 5] and [11].
+ */
 public class PartitionEqualSubsetSum {
 
     public static void main(final String[] args) {
@@ -17,7 +28,7 @@ public class PartitionEqualSubsetSum {
         if (nums.length == 1) return false;
 
         final int sum = Arrays.stream(nums)
-                .sum();
+                              .sum();
 
         if (sum % 2 == 1) return false; // if sum is odd, return false
 
